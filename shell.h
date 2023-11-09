5523cb8f9ee36c6fb416c3ed693c70ecb5ac6f8c
Egba Frank <Egbafrank72@gmail.com>
2023-11-09 22:24:33 +0000

    Added main file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#endif /*SHELL_H*/
